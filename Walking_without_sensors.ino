108345d637c32ac701c533edd9f1185157c5d8c0
N3O256 <90545005+N3O256@users.noreply.github.com>
2021-09-20 20:16:54 +0200

    enable to walk backwards

--- Walking_without_sensors.ino
+++ Walking_without_sensors.ino
@@ -25,8 +25,236 @@ void setup() {
 void loop() {
   homePos();
   //forward(1);
+  //backward(1);
 }
 
+//----------------backward---------------
+void backward(int number){
+  pos1b();
+  delay(timeDelay);
+  for(int i = 0; i < number; i++){
+    step1b();
+    delay(timeDelay);
+    pos2b();
+    delay(timeDelay);
+    step2b();
+    delay(timeDelay);
+    pos3b();
+    delay(timeDelay);
+    step3b();
+    delay(timeDelay);
+    pos4b();
+    delay(timeDelay);
+    step4b();
+    delay(timeDelay);
+  }
+}
+
+void step4b(){
+  //front right
+  angle[8] = 230;
+  map_values(8);
+  s.setPWM(8, 0, pwm[8]);
+  delay(200);
+  
+  angle[9] = 69;
+  angle[10] = 126;
+  map_values(9);
+  map_values(10);
+  s.setPWM(9, 0, pwm[9]);
+  s.setPWM(10, 0, pwm[10]);
+  delay(200);
+
+  angle[8] = 199;
+  map_values(8);
+  s.setPWM(8, 0, pwm[8]);
+  delay(200);
+}
+
+void pos4b(){
+  //back right
+  angle[6] = 119;
+  angle[4] = 177;
+  angle[5] = 55;
+
+  //back left
+  angle[14] = 108;
+  angle[12] = 195;
+  angle[13] = 62;
+
+  //front right
+  angle[2] = 141;
+  angle[0] = 191;
+  angle[1] = 59;
+
+  //front left
+  angle[10] = 144;
+  angle[8] = 195;
+  angle[9] = 96;
+
+  for(int i = 0; i < 15; i++){
+    map_values(i);
+  }
+
+  for(int i = 0; i < 15; i++){
+    s.setPWM(i, 0, pwm[i]);
+  }
+}
+
+void step3b(){
+  //front right
+  angle[4] = 230;
+  map_values(4);
+  s.setPWM(4, 0, pwm[4]);
+  delay(200);
+  
+  angle[5] = 69;
+  angle[6] = 126;
+  map_values(5);
+  map_values(6);
+  s.setPWM(5, 0, pwm[5]);
+  s.setPWM(6, 0, pwm[6]);
+  delay(200);
+
+  angle[4] = 199;
+  map_values(4);
+  s.setPWM(4, 0, pwm[4]);
+  delay(200);
+}
+
+void pos3b(){
+  //back right
+  angle[6] = 94;
+  angle[4] = 193;
+  angle[5] = 60;
+
+  //back left
+  angle[14] = 141;
+  angle[12] = 191;
+  angle[13] = 59;
+
+  //front right
+  angle[2] = 109;
+  angle[0] = 194;
+  angle[1] = 62;
+
+  //front left
+  angle[10] = 119;
+  angle[8] = 177;
+  angle[9] = 55;
+
+  for(int i = 0; i < 15; i++){
+    map_values(i);
+  }
+
+  for(int i = 0; i < 15; i++){
+    s.setPWM(i, 0, pwm[i]);
+  }
+}
+
+void step2b(){
+  //front right
+  angle[0] = 235;
+  map_values(0);
+  s.setPWM(0, 0, pwm[0]);
+  delay(200);
+  
+  angle[1] = 64;
+  angle[2] = 118;
+  map_values(1);
+  map_values(2);
+  s.setPWM(1, 0, pwm[1]);
+  s.setPWM(2, 0, pwm[2]);
+  delay(200);
+
+  angle[0] = 197;
+  map_values(0);
+  s.setPWM(0, 0, pwm[0]);
+  delay(200);
+}
+
+void pos2b(){
+  //back right
+  angle[6] = 85;
+  angle[4] = 193;
+  angle[5] = 60;
+
+  //back left
+  angle[14] = 148;
+  angle[12] = 173;
+  angle[13] = 55;
+
+  //front right
+  angle[2] = 140;
+  angle[0] = 198;
+  angle[1] = 86;
+
+  //front left
+  angle[10] = 153;
+  angle[8] = 199;
+  angle[9] = 73;
+
+  for(int i = 0; i < 15; i++){
+    map_values(i);
+  }
+
+  for(int i = 0; i < 15; i++){
+    s.setPWM(i, 0, pwm[i]);
+  }
+}
+
+void step1b(){
+  //front right
+  angle[12] = 235;
+  map_values(12);
+  s.setPWM(12, 0, pwm[12]);
+  delay(200);
+  
+  angle[13] = 77;
+  angle[14] = 135;
+  map_values(13);
+  map_values(14);
+  s.setPWM(13, 0, pwm[13]);
+  s.setPWM(14, 0, pwm[14]);
+  delay(200);
+
+  angle[12] = 199;
+  map_values(12);
+  s.setPWM(12, 0, pwm[12]);
+  delay(200);
+}
+
+void pos1b(){
+  //back right
+  angle[6] = 119;
+  angle[4] = 177;
+  angle[5] = 55;
+
+  //back left
+  angle[14] = 108;
+  angle[12] = 195;
+  angle[13] = 62;
+
+  //front right
+  angle[2] = 141;
+  angle[0] = 191;
+  angle[1] = 59;
+
+  //front left
+  angle[10] = 126;
+  angle[8] = 199;
+  angle[9] = 69;
+
+  for(int i = 0; i < 15; i++){
+    map_values(i);
+  }
+
+  for(int i = 0; i < 15; i++){
+    s.setPWM(i, 0, pwm[i]);
+  }
+}
+
+//----------------forward---------------
 void forward(int number){
   pos1();
   delay(timeDelay);
@@ -47,7 +275,7 @@ void forward(int number){
     delay(timeDelay);
   }
 }
-
+  
 void step4(){
   //front right
   angle[12] = 235;
